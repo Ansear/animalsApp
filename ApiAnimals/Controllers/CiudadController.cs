544ae6d941386b003c9d4cb892ac620c00c62484
Ansear <107230475+Ansear@users.noreply.github.com>
2023-10-09 13:02:48 -0500

    feat: :wrench: Add Controller RazaController

--- ApiAnimals/Controllers/CiudadController.cs
+++ ApiAnimals/Controllers/CiudadController.cs
@@ -38,4 +38,62 @@ public class CiudadController : BaseController
         }
         return _mapper.Map<CiudadDto>(ciudad);
     }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<Ciudad>> Post(CiudadDto ciudadDto)
+    {
+        var ciudad = _mapper.Map<Ciudad>(ciudadDto);
+        _unitOfWork.Ciudades.Add(ciudad);
+        await _unitOfWork.SaveAsync();
+        if(ciudad == null)
+        {
+            return BadRequest();
+        }
+        ciudadDto.Id = ciudad.Id;
+        return CreatedAtAction(nameof(ciudad), new { id = ciudad.Id }, ciudadDto);
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<CiudadDto>> Put(int id, [FromBody]CiudadDto ciudadDto)
+    {
+        var ciudad = _unitOfWork.Ciudades.GetByIdAsync(id);
+        if(ciudadDto == null)
+        {
+            return BadRequest();
+        }
+        if(ciudad.Id == 0)
+        {
+            id = ciudad.Id;
+        }
+        if(ciudad.Id != id)
+        {
+            return BadRequest();
+        }
+        var ciudades = _mapper.Map<Ciudad>(ciudadDto);
+        _unitOfWork.Ciudades.Update(ciudades);
+        await _unitOfWork.SaveAsync();
+        return ciudadDto;
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var ciudad = await _unitOfWork.Ciudades.GetByIdAsync(id);
+        if(ciudad == null)
+        {
+            return NotFound();
+        }
+        _unitOfWork.Ciudades.Remove(ciudad);
+        await _unitOfWork.SaveAsync();
+        return NoContent();
+
+    }
 }
